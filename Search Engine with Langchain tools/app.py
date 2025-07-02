83c3065648518585489033265f38875031893617
jay-1799 <patelj1799@gmail.com>
2025-07-02 16:16:22 -0400

    WIP: save work before history rewrite

--- Search Engine with Langchain tools/app.py
+++ Search Engine with Langchain tools/app.py
@@ -7,35 +7,7 @@ from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
 
-arxiv_wrapper = ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=200)
-arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
-
-api_wrapper = WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
-wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
-
-search=DuckDuckGoSearchRun(name="Search")
-
-st.title("lancghain - Chat with search")
-
-st.sidebar.title("Settings")
-api_key=st.sidebar.text_input("Enter your groq API key:",type="password")
-
-if "messages" not in st.session_state:
-    st.session_state["messages"]=[
-        {"role":"assisstant","content":"Hi,I'm a chatbot who can search the web.How can i help you"}
-    ]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg['content'])
-
-if prompt:=st.chat_input(placeholder="What is machine learning?"):
-    st.session_state.messages.append({"role":"user","content":prompt})
-    st.chat_message("user").write(prompt)
-
-    llm = ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
-    tools=[search,arxiv,wiki]
-    
-    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
+.
 
     with st.chat_message("assisstant"):
         st_cb=StreamlitCallbackHandler(st.container(),expand_new_thougts=False)
